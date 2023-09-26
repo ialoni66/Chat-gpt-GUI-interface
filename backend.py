771736bf9690a3ed551f2394239ee4a8439333be
ilai <ilai.aloni@gmail.com>
2023-09-26 16:37:58 -0700

    Chat Gpt interface complete

--- backend.py
+++ backend.py
@@ -1,2 +1,17 @@
+import openai
+
 class Chatbot:
-    pass
+    def __init__(self):
+        openai.api_key = "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"
+
+    def get_response(self,user_input):
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=3000,
+            temperature=0.5,
+        ).choices[0].text
+        return response
+
+
+
